9f053b9337cf49f6d0fae1603a799240e33ab079
root <priyanshi.jajoo@sjsu.edu>
2020-03-10 23:22:08 -0700

    Adding Primary Processor-Based VM-Execution Controls

--- CMPE283/cmpe283-1.c
+++ CMPE283/cmpe283-1.c
@@ -1,3 +1,5 @@
+/*
+*/
 /*  
  *  cmpe283-1.c - Kernel module for CMPE283 assignment 1
  */
@@ -11,7 +13,11 @@
  * Model specific registers (MSRs) by the module.
  * See SDM volume 4, section 2.1
  */
-#define IA32_VMX_PINBASED_CTLS	0x481
+#define IA32_VMX_PINBASED_CTLS 0x481
+#define IA32_VMX_PROCBASED_CTLS	0x482
+//#define IA32_VMX_PROCBASED_CTLS2 0x48B
+//#define IA32_VMX_EXIT_CTLS 0x483
+//#define IA32_VMX_ENTRY_CTLS 0x484
 
 /*
  * struct caapability_info
@@ -37,6 +43,36 @@ struct capability_info pinbased[5] =
 	{ 6, "Activate VMX Preemption Timer" },
 	{ 7, "Process Posted Interrupts" }
 };
+/*
+ * Pinbased capabilities
+ * See SDM volume 3, section 24.6.2
+ */
+struct capability_info procbased[21] =
+{
+	{2, "Interrupt-window exiting"},
+	{3, "Use TSC offsetting"},
+	{7, "HLT exiting"},
+	{9, "INVLPG exiting"},
+	{10, "MWAIT exiting"},
+	{11, "RDPMC exiting"},
+	{12, "RDTSC exiting"},
+	{15, "CR3-load exiting"},
+	{16, "CR3-store exiting"},
+	{19, "CR8-load exiting"},
+	{20, "CR8-store exiting"},
+	{21, "Use TPR shadow"},
+	{22, "NMI-window exiting"},
+	{23, "MOV-DR exiting"},
+	{24, "Unconditional I/O exiting"},
+	{25, "Use I/O bitmaps"},
+	{27, "Monitor trap flag"},
+	{28, "Use MSR bitmaps"},
+	{29, "MONITOR exiting"},
+	{30, "PAUSE exiting"},
+	{31, "Activate secondary controls"}
+};
+
+
 
 /*
  * report_capability
@@ -85,6 +121,9 @@ detect_vmx_features(void)
 	pr_info("Pinbased Controls MSR: 0x%llx\n",
 		(uint64_t)(lo | (uint64_t)hi << 32));
 	report_capability(pinbased, 5, lo, hi);
+	pr_info("Procbased Controls MSR: 0x%llx\n",
+		(uint64_t)(lo | (uint64_t)hi << 32));
+	report_capability(procbased, 21, lo, hi);
 }
 
 /*
@@ -98,7 +137,7 @@ detect_vmx_features(void)
 int
 init_module(void)
 {
-	printk(KERN_INFO "CMPE 283 Assignment 1 Module Start\n");
+	printk(KERN_INFO "Priyanshi-CMPE 283 Assignment 1 Module Start\n");
 
 	detect_vmx_features();
 
